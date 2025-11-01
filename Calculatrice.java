cafe18a02b5cc23ccd8d6f75bee13629ffd9b50a
ayoub404-mak <ayoub151539@gmail.com>
2025-11-01 18:55:21 +0100

    exercice4

---
+++ Calculatrice.java
@@ -0,0 +1,17 @@
+class Calculatrice{
+
+
+    public static int addition(int a, int b) {
+        return a+b;
+
+    };
+    public static double addition(double a, double b) {
+        return a+b;
+
+    };
+    public static int addition(int a, int b,int c) {
+        return a+b+c;
+
+    };
+
+}
---
+++ mainCalculatrice.java
@@ -0,0 +1,10 @@
+
+public class mainCalculatrice {
+    public static void main(String[] args) {
+
+        System.out.println("a+b= "+Calculatrice.addition(1,2));
+        System.out.println("a+b= "+Calculatrice.addition(0.4,1.5));
+        System.out.println("a+b= "+Calculatrice.addition(1,2,3));
+
+    }
+}
